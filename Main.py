9d5335a40dc4e30410931c0e00ac027a43a8bd6f
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-01-12 15:52:30 -0700

    Add files via upload
    Added text label that tells you what file is loaded. Fixed Playbutton a little and changed its name (from PlayButton).

--- Main.py
+++ Main.py
@@ -4,14 +4,16 @@ from os.path import expanduser
 
 from simpleaudio import WaveObject
 
-from PlayButton import PlayButton
+from Playbutton import Playbutton
 from pydub import AudioSegment
 
 
 def create_root_window():
     root = Tk()
-    window_width = 300
-    window_height = 200
+    window_width = 302
+    window_height = 50
+    root.minsize(window_width, window_height)
+    # TODO: when you change root window size, buttons + label get bigger
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x_coord = (screen_width/2) - (window_width/2)
@@ -19,21 +21,27 @@ def create_root_window():
     root.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coord, y_coord))
     root.title("Sonic Surge")
 
-    play_btn = PlayButton(root, text="Play mp3")  # TODO: say "No file loaded" if button is clicked without file loaded
-    choose_btn = Button(root, text="Choose mp3", command=lambda: change_audio(play_btn))
-    play_btn.pack(side=LEFT, anchor=SW, padx=7, pady=8)
-    choose_btn.pack(side=RIGHT, anchor=SE, padx=9, pady=8)
+    play_btn = Playbutton(root, text="Play mp3")  # TODO: say "No file loaded" if button is clicked without file loaded
+    choose_btn = Button(root, text="Choose mp3", command=lambda: change_audio_file(play_btn, file_label))
+    # TODO: pause button
+    play_btn.grid(column=0, row=1)
+    choose_btn.grid(column=2, row=1)
+
+    file_label = Label(root, text="No file loaded.", justify=CENTER)
+    file_label.grid(row=0, columnspan=2)  # TODO: fix label and button layout
 
 
 def get_file_path_from_user():
     return filedialog.askopenfilename(initialdir=expanduser("~/Music/"), filetypes=[("MP3 files", "*.mp3")])
 
 
-def change_audio(play_btn):
+def change_audio_file(play_btn, file_label):
     play_btn.stop()
     file_path = get_file_path_from_user()
     if file_path != '':
         play_btn.set_sound(get_wave_object(file_path))
+        file_name = file_path[file_path.rfind("/")+1:]
+        file_label.config(text="File loaded: " + file_name)
 
 
 def get_wave_object(audio_file_path):
