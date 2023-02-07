a63044f2aea7cff4f66a5b8fdb92cc42c17ee251
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-02-06 19:46:34 -0700

    Add files via upload
    My first ever unit test: checking that a sound plays!

--- tests/test_SoundPlayer.py
+++ tests/test_SoundPlayer.py
@@ -1,15 +1,32 @@
 import unittest
 
-from source.SoundPlayer import SoundPlayer as SoundPlayer
+import pydub
+from pydub import AudioSegment
+from simpleaudio import WaveObject
+
 from source.Sound import Sound as Sound
+from source.SoundBuilder import SoundBuilder as SoundBuilder
+from source.SoundPlayer import SoundPlayer as SoundPlayer
 
 
 class MyTestCase(unittest.TestCase):
 
-    def sound_plays(self):
+    def test_sound_plays(self):
+        seg = AudioSegment.from_file("/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Bob Marley - Is This Love.mp3", "mp3")
+        wave_obj = WaveObject(seg.raw_data, seg.channels, seg.sample_width, seg.frame_rate)
+        sound = Sound(wave_obj=wave_obj)
         player = SoundPlayer()
-        self.assertEqual(True, False)  # add assertion here
+        player.sound = sound
+        player.play()
+        self.assertTrue(player.play_obj.is_playing())
+
+
+def player_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(MyTestCase('test_sound_plays'))
+    return suite
 
 
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(player_suite())
