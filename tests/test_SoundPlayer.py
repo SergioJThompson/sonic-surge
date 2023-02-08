f20debdd9eeb66602d8e69f78e70cc9fc7c3f370
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-02-07 23:48:57 -0700

    Add files via upload
    Added five new tests for the SoundPlayer.

--- tests/test_SoundPlayer.py
+++ tests/test_SoundPlayer.py
@@ -1,24 +1,59 @@
+import time
 import unittest
 
-import pydub
 from pydub import AudioSegment
 from simpleaudio import WaveObject
 
 from source.Sound import Sound as Sound
-from source.SoundBuilder import SoundBuilder as SoundBuilder
 from source.SoundPlayer import SoundPlayer as SoundPlayer
+
+
+def play_file(player, path):
+    seg = AudioSegment.from_file(path)
+    wave_obj = WaveObject(seg.raw_data, seg.channels, seg.sample_width, seg.frame_rate)
+    player.sound = Sound(wave_obj=wave_obj)
+    player.play()
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_sound_plays(self):
-        seg = AudioSegment.from_file("/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Bob Marley - Is This Love.mp3", "mp3")
-        wave_obj = WaveObject(seg.raw_data, seg.channels, seg.sample_width, seg.frame_rate)
-        sound = Sound(wave_obj=wave_obj)
         player = SoundPlayer()
-        player.sound = sound
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Bob Marley - Is This Love.mp3")
+        self.assertTrue(player.play_obj.is_playing())
+
+    def test_long_sound_plays(self):
+        player = SoundPlayer()
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Nina Simone - Sinnerman.mp3")
+        self.assertTrue(player.play_obj.is_playing())
+
+    def test_short_sound_plays(self):
+        player = SoundPlayer()
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Mario coin.mp3")
+        self.assertTrue(player.play_obj.is_playing())
+
+    def test_playback_stops(self):
+        player = SoundPlayer()
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Bob Marley - Is This Love.mp3")
+        player.stop_if_playing()
+        time.sleep(0.2)     # Stopping playback takes a moment, it seems, so we wait.
+        self.assertFalse(player.play_obj.is_playing())
+
+    def test_play_stop_play_again(self):
+        player = SoundPlayer()
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Bob Marley - Is This Love.mp3")
+        player.stop_if_playing()
+        time.sleep(0.2)
         player.play()
         self.assertTrue(player.play_obj.is_playing())
+
+    def test_no_overlapping_playbacks(self):
+        player = SoundPlayer()
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Bob Marley - Is This Love.mp3")
+        first_playback = player.play_obj
+        play_file(player, "/Users/sergiojthompson/Documents/programs/sonic surge/test_files/Joy Division - Disorder.mp3")
+        time.sleep(0.2)
+        self.assertFalse(first_playback.is_playing())
 
 
 def player_suite():
